74c95bfdccf0ab399b96fa2db766090689dfc0df
Eric Petsopoulos <epetsopoulos@gmail.com>
2022-04-09 03:24:25 +0100

    smart child

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -8,16 +8,18 @@ const openai = new OpenAIApi(configuration);
 export default async function (req, res) {
   const completion = await openai.createCompletion("text-davinci-002", {
     prompt: generatePrompt(req.body.question),
-    temperature: 0.6, // TODO add temp slider UI
-    max_tokens: 350,
+    temperature: 0, // TODO add temp slider UI
+    max_tokens: 450,
   });
   
   res.status(200).json({ result: completion.data.choices[0].text });
 }
 
+// a child
+
 function generatePrompt(question) {
 
-  return `Answer my question in a way that a child would understand.
+  return `Answer my question in a way that a smart child would understand.
 
 My question: ${question}
 Your answer:`;
